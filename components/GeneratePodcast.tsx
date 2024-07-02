d68fd2893e812eb23c847ec0c7d5a4905f51a84c
samtaylek17 <ogunsusitemitayo99@gmail.com>
2024-07-02 12:57:10 +0100

    Create Podcast page

---
+++ components/GeneratePodcast.tsx
@@ -0,0 +1,9 @@
+/** @format */
+
+import React from 'react';
+
+const GeneratePodcast = () => {
+	return <div>GeneratePodcast</div>;
+};
+
+export default GeneratePodcast;
---
+++ convex/auth.config.ts
@@ -0,0 +1,11 @@
+/** @format */
+
+// https://rich-cougar-17.clerk.accounts.dev
+export default {
+	providers: [
+		{
+			domain: 'https://rich-cougar-17.clerk.accounts.dev',
+			applicationID: 'convex',
+		},
+	],
+};
